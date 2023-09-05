8f68e74f28965bebffb9ecf2a7c9e75f14c95f38
bururen <144135577+bururen@users.noreply.github.com>
2023-09-05 21:44:00 +0800

    Add files via upload

---
+++ xry/乳腺癌随访小程序/pages/index/index.js
@@ -0,0 +1,24 @@
+// index.js
+// 获取应用实例
+Page({
+  data: {
+      username: '',
+      password: '',
+      text:'',
+  },
+inputUsername(e) {
+  this.setData({
+    username: e.detail.value,
+  })
+},
+inputPassword(e) {
+  this.setData({
+    password: e.detail.value,
+  })
+},
+toregiste(){
+  wx.navigateTo({
+    url: '../register',
+  })
+},
+})
